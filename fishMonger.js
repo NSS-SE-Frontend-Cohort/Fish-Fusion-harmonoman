47d148e0337b72140b116edd8eef3fcd203d91a2
Mark Bond <markbond615@gmail.com>
2025-01-27 11:19:45 -0500

    Added mongerInventory function to fishMonger.js

---
+++ fishMonger.js
@@ -0,0 +1,24 @@
+const { boatInventory } = require("./fishingBoat.js");
+
+const mongerInventory = (chefBudget) => {
+    // Monger filter
+    const mongerArray = [];
+    const todaysCatch = boatInventory();
+    for (const fish of todaysCatch) {
+        if (fish.amount >= 10 && fish.price <= 7.50) {
+            mongerArray.push(fish);
+        }
+    }
+
+    // Chef filter
+    const chefArray = [];
+    for (const item of mongerArray) {
+        if (item.price <= chefBudget) {
+            chefArray.push(item);
+        }
+    }
+
+    return chefArray;
+}
+
+module.exports = { mongerInventory };
